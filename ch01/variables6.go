0ed6d6caf2ec7ec2589b6c6844df8a1dc90ab4b3
Srikanth Devarashetty <ml2dkanth@yahoo.com>
2021-05-25 17:49:33 +0530

    variables of different types

---
+++ ch01/variables6.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message string
+	var a, b, c int
+	a = 1
+
+	message = "Hello World!"
+
+	fmt.Println(message, a, b, c)
+	
+	var message2 = "Hello World!"
+	var aa, bb, cc int = 1, 2, 3
+
+	fmt.Println(message2, aa, bb, cc)
+
+}
